8136c0eea82c19df37023c38d257881e1acd1d76
ausstinh <47186695+ausstinh@users.noreply.github.com>
2019-03-01 08:56:19 -0700

    Add files via upload

---
+++ showAddForm.php
@@ -0,0 +1,11 @@
+
+
+<div class="form-container">
+<h2>Make A Post</h2>
+<p>Fill out all of the fields and submit</p>
+<form action="processAddItem.php">
+    Post Title:<input type="text" name="PostTitle"></input><br>
+    Post Information:<textarea rows="5" cols="50" name="PostInformation"></textarea><br>
+    <button type="submit">Add</button>
+</form>
+</div>
---
+++ showMainWelcome.php
@@ -0,0 +1,6 @@
+
+ 
+<div class="form-container">
+<h1>Austin's Blog</h1>
+<p>This is a blog dedicated to Austin Harvey</p>
+</div>
